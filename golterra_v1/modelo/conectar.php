047ab9e9235c69baecb920c5681b476857c2003a
Maycol Meza <mychael.jared29@gmail.com>
2017-12-01 00:05:00 -0500

    sdfs

--- golterra_v1/modelo/conectar.php
+++ golterra_v1/modelo/conectar.php
@@ -5,12 +5,11 @@ $server = $url["host"];
 $usernameserver = $url["user"];
 $passwordserver = $url["pass"];
 $db = substr($url["path"], 1);
-
 class Conectar{
 	public static function conexion(){
 		
 		try{
-			$conexion = new PDO('mysql:host=$server; dbname=$db','$usernameserver','$passwordserver');
+			$conexion = new PDO("mysql:host=$server; dbname=$db",$usernameserver,$passwordserver);
 			$conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$conexion -> exec("SET CHARACTER SET UTF8");
 			
