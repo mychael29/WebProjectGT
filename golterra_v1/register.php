23a046b465889b8e411a2a80ed25e989cea13b83
Maycol Meza <mychael.jared29@gmail.com>
2017-11-30 23:56:11 -0500

    fdsf

--- golterra_v1/register.php
+++ golterra_v1/register.php
@@ -17,7 +17,29 @@
 <body>
 
 <?php
-include($_SERVER . "config.php");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$usernameserver = $url["user"];
+$passwordserver = $url["pass"];
+$db = substr($url["path"], 1);
+class Conectar{
+	public static function conexion(){
+		
+		try{
+			$conexion = new PDO('mysql:host=$server; dbname=$db','$usernameserver','$passwordserver');
+			$conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$conexion -> exec("SET CHARACTER SET UTF8");
+			
+	}catch(Exception $e){
+		
+		die("Error " . $e->getMessage());
+		echo "Linea del error " . $e->getLine();
+	}
+	
+	return $conexion;
+	}
+}
 //require $server . '/sesion.php';
 session_start();
 $errors = array(); 
