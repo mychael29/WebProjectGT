9bd5dd9b0bfded3c886c2fc1bd0eedae35925eb7
Maycol Meza <mychael.jared29@gmail.com>
2017-12-01 04:56:51 -0500

    dsfds

--- golterra_v1/register_movil.php
+++ golterra_v1/register_movil.php
@@ -1,21 +1,37 @@
 <?php
+//error_reporting(E_ALL);
+include('config.php') ;
 
-require_once($server . "/modelo/conectar.php");
+try{
+    $conexion = new PDO($host,$usernameserver,$passwordserver);
+    $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion -> exec("SET CHARACTER SET UTF8");
+}catch(Exception $e){
+    die("Error " . $e->getMessage());
+    echo "Linea del error " . $e->getLine();
+}
+
 	
-	class SignupUser {
+	if(isset($_POST['email'],$_POST['password'],$_POST['nombres'])) {
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$nombres = $_POST['nombres'];
+		$image = $_POST['photo'];
 		
-		private $db;
-        private $conexion;
-		
-		function __construct() {
-			$this -> db = new Conectar();
-			$this -> conexion = $this->db->conexion();
-        }
+		if($image!="no imagen"){
+			$path  = "image_profile/$nombres.jpg"; 
+			$url_image = "image_profile/".$nombres.".jpg";//agregarle el ID al nombre de la imagen
+			file_put_contents($path,base64_decode($image));
+			//$bytesArchivo=file_get_contents($path);//para guardar la imagen en la tabla de la bbdd
+		}else{
+			$url_image = "sin imagen";
+		}
         
-		public function does_user_exist($email,$password,$url_image,$nombres)
-		{
-            $query = "Select * from usuarios where email='$email'";
-            $result = $this -> conexion->prepare($query);
+		if(!empty($email) && !empty($password) && !empty($nombres)){
+			
+			$encrypted_password = md5($password);
+			$query = "Select * from usuarios where email='$email'";
+            $result = $conexion->prepare($query);
             $result->execute();
             //$result = mysqli_query($this->conexion, $query);
             
@@ -27,7 +43,7 @@ require_once($server . "/modelo/conectar.php");
 			}else{
 				//registro
                 $query = "insert into usuarios (email, password, photo, nombres) values (?,?,?,?)";
-				$inserted = $this->conexion->prepare($query);
+				$inserted = $conexion->prepare($query);
 				
 				//$inserted->bindParam('ssss',$email,$password,$url_image,$nombres);//estaba con bind_param
 				$inserted->bindParam(1, $email, PDO::PARAM_STR); 
@@ -51,7 +67,7 @@ require_once($server . "/modelo/conectar.php");
 
 					try {
 						// Preparar sentencia
-						$comando = $this->conexion->prepare($query);
+						$comando = $conexion->prepare($query);
 						// Ejecutar sentencia preparada
 						$comando->execute(array($email));
 						// Capturar primera fila del resultado
@@ -70,39 +86,11 @@ require_once($server . "/modelo/conectar.php");
 				}
 				echo json_encode($json);
 				//mysqli_close($this -> conexion); // buscar otra forma de cerrar la conexion, y si es necesario aqui
-			}    
-			
-		}
-		
-	}
-	
-	
-	$signupUser = new SignupUser();
-	if(isset($_POST['email'],$_POST['password'],$_POST['nombres'])) {
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-		$nombres = $_POST['nombres'];
-		$image = $_POST['photo'];
-		
-		if($image!="no imagen"){
-			$path  = "image_profile/$nombres.jpg"; 
-			$url_image = "image_profile/".$nombres.".jpg";//agregarle el ID al nombre de la imagen
-			file_put_contents($path,base64_decode($image));
-			//$bytesArchivo=file_get_contents($path);//para guardar la imagen en la tabla de la bbdd
-		}else{
-			$url_image = "sin imagen";
-		}
-        
-		if(!empty($email) && !empty($password) && !empty($nombres)){
-			
-			$encrypted_password = md5($password);
-			$signupUser-> does_user_exist($email,$password,$url_image,$nombres);
+			}
 			
 		}else{
 			echo json_encode("debe escribir ambas entradas");
 		}
 		
-	}else{
-		echo "funciona";
 	}
 ?>
