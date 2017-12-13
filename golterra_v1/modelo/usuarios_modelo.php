2109b8363a0ac29682c2bd89fbc15176e1ec6ff7
Maycol Meza <mychael.jared29@gmail.com>
2017-12-12 23:39:05 -0500

    kfjm

--- golterra_v1/modelo/usuarios_modelo.php
+++ golterra_v1/modelo/usuarios_modelo.php
@@ -13,7 +13,31 @@
 
 	$usuarios=array();
 	
-	require('paginacion.php');
+	$tamagno_paginas=3;
+	
+	  if(isset($_GET["pagina"])){
+		  
+		  if ($_GET["pagina"]==1){
+			  
+			  header("Location:index_users.php");
+			  
+		  }else{
+			  
+			  $pagina=$_GET["pagina"];
+			  
+		  }
+		  
+	  }else{
+		  
+		  $pagina=1;
+	  }
+	  
+	  $empezar_desde=($pagina-1)*$tamagno_paginas;
+	  $sql_total="SELECT username, email, nombres FROM usuarios";
+	  $resultado=$conexion->prepare($sql_total);
+	  $resultado->execute(array());
+	  $num_filas=$resultado->rowCount();
+	  $total_paginas=ceil($num_filas/$tamagno_paginas);
 	
 	$consulta=$conexion->query("SELECT * FROM usuarios LIMIT $empezar_desde, $tamagno_paginas");
 	
