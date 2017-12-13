5692f8d520722dcb1d5bb6261f16c266e7a749f0
Maycol Meza <mychael.jared29@gmail.com>
2017-12-13 14:38:50 -0500

    jkk

---
+++ golterra_v1/upload_image_profile.php
@@ -0,0 +1,24 @@
+<?php
+include('config.php') ;
+
+try{
+    $conexion = new PDO($host,$usernameserver,$passwordserver);
+    $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion -> exec("SET CHARACTER SET UTF8");
+}catch(Exception $e){
+    die("Error " . $e->getMessage());
+    echo "Linea del error " . $e->getLine();
+}
+
+if(isset($_POST['photo'])) {
+ 
+    $nombres = $_POST['nombres'];
+    $image = $_POST['photo'];
+    
+    $path  = "json/image_profile/$nombres.jpg"; 
+    $url_image = "image_profile/".$nombres.".jpg";//agregarle el ID al nombre de la imagen
+    file_put_contents($path,base64_decode($image));
+    //$bytesArchivo=file_get_contents($path);//para guardar la imagen en la tabla de la bbdd
+    
+}
+?>
