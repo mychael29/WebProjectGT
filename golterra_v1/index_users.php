fd09603227c39982f62e79b77abafbb51faff3d8
Maycol Meza <mychael.jared29@gmail.com>
2017-12-13 01:09:45 -0500

    kk

--- golterra_v1/index_users.php
+++ golterra_v1/index_users.php
@@ -102,13 +102,31 @@
       
       <?php
       
-      require('modelo/usuarios_modelo.php');
+      require_once("../config.php");
+      try{
+        $conexion = new PDO($host,$usernameserver,$passwordserver);
+        $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conexion -> exec("SET CHARACTER SET UTF8");
+      }catch(Exception $e){
+        die("Error " . $e->getMessage());
+        echo "Linea del error " . $e->getLine();
+      }
+      $usuarios=array();
+      
+      require("paginacion.php");
+      
+      $consulta=$conexion->query("SELECT * FROM usuarios LIMIT $empezar_desde, $tamagno_paginas");
+      
+      while($filas=$consulta->fetch(PDO::FETCH_ASSOC)){
+        
+        $usuarios[]=$filas;
+      }
       
       $matrizUsuario = $usuarios;
       
       require('vista/usuarios_view.php');
-    
-       ?>
+      
+      ?>
       </div>
     </div>
   </div>
