37dda89eb33fb370d50d02ba9333845e1d95dd7b
Maycol Meza <mychael.jared29@gmail.com>
2017-12-12 20:49:59 -0500

    dsds

--- golterra_v1/index.php
+++ golterra_v1/index.php
@@ -86,7 +86,7 @@
                                                     <div class="row">
                                                         <div class="col-md-5">
                                                             <img src="<?php echo 'https://arcane-ravine-59770.herokuapp.com/json/' . $_SESSION['username']['photo']; ?>"
-                                                                alt="Alternate Text" class="img-responsive" />
+                                                                alt="Alternate Text" class="img-responsive img-thumbnail" />
                                                             <p class="text-center small">
                                                                 <a href="#">Cambiar foto</a></p>
                                                         </div>
