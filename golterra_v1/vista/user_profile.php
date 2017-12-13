bdd3a95a3763fe835e012aabb96eed38bf9931ff
Maycol Meza <mychael.jared29@gmail.com>
2017-12-12 19:47:12 -0500

    dsQ

--- golterra_v1/vista/user_profile.php
+++ golterra_v1/vista/user_profile.php
@@ -168,7 +168,7 @@
 
 
         </div>
-      <div class="col-sm-2"><a href="/users" class="pull-right"><div class="avatar" style="<?php echo 'background-image: url("https://arcane-ravine-59770.herokuapp.com/json/' . $_SESSION['username']['photo'].'")'; ?>" ></div></a>
+      <div class="col-sm-2"><a href="/users" class="pull-right"><div class="avatar" style="<?php echo 'background-image: url(https://arcane-ravine-59770.herokuapp.com/json/' . $_SESSION['username']['photo'] . ')'; ?>" ></div></a>
       
       <!--<img title="profile image" class="avatar img-responsive" src=" ">-->
         </div>
