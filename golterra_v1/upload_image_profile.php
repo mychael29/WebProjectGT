ced9cf8aedba5fafa565f4662e9231ff1445c278
Maycol Meza <mychael.jared29@gmail.com>
2017-12-14 23:37:46 -0500

    corrigiendo detalles agregando codigo

--- golterra_v1/upload_image_profile.php
+++ golterra_v1/upload_image_profile.php
@@ -14,11 +14,14 @@ if(isset($_POST['photo'])) {
  
     $nombres = $_POST['nombres'];
     $image = $_POST['photo'];
-    
+
     $path  = "json/image_profile/$nombres.jpg"; 
     $url_image = "image_profile/".$nombres.".jpg";//agregarle el ID al nombre de la imagen
     file_put_contents($path,base64_decode($image));
     //$bytesArchivo=file_get_contents($path);//para guardar la imagen en la tabla de la bbdd
-    
+    $query = "UPDATE usuarios SET photo = '$url_image' WHERE nombres = '$nombres'";
+    $inserted = $conexion->prepare($query);
+    $inserted->execute();
+
 }
 ?>
