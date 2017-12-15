e3c8f32e576c91b6b13d40577898dbacc2ecd6ba
Maycol Meza <mychael.jared29@gmail.com>
2017-12-15 01:05:15 -0500

    corrigiendo detalles

--- golterra_v1/sesion_register.php
+++ golterra_v1/sesion_register.php
@@ -26,6 +26,7 @@ if (isset($_POST['reg_user'])) {
             $email = $_POST['email'];
             $password_1 = $_POST['password_1'];
             $password_2 = $_POST['password_2'];
+            $image = "sin imagen";
     
             // Validacion del formulario
             if (empty($username)) { array_push($errors, "Username es requerido"); }
@@ -53,18 +54,17 @@ if (isset($_POST['reg_user'])) {
                     //mysqli_close($this -> conexion); // buscar otra forma de cerrar la conexion, y si es necesario aqui
                 }else{
                     //registro
-                    $query = "insert into usuarios (username,email,password,nombres,photo) values (?,?,?,?,?)";
+                    $query = "insert into usuarios (username,email,password,photo,nombres) values (?,?,?,?,?)";
                     $inserted = $conexion->prepare($query);
                     
                     //$inserted->bindParam('ssss',$email,$password,$url_image,$nombres);//estaba con bind_param
                     $inserted->bindParam(1, $username, PDO::PARAM_STR);
                     $inserted->bindParam(2, $email, PDO::PARAM_STR); 
                     $inserted->bindParam(3, $password_1, PDO::PARAM_STR);
-                    $inserted->bindParam(4, $nombres, PDO::PARAM_STR);
-                    $inserted->bindParam(5, "sin imagen", PDO::PARAM_STR);
+                    $inserted->bindParam(4, $image, PDO::PARAM_STR);
+                    $inserted->bindParam(5, $nombres, PDO::PARAM_STR);
                     
                 
-                    
                     if($inserted->execute()){
                         //$json['success'] = 'Cuenta creada';
                         $query = "SELECT username,email,nombres,photo FROM usuarios WHERE email='$email'";
