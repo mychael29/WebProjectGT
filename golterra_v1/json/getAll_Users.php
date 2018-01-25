38662c669d3c0a152bf49542bff188b6a80bf104
Maycol Meza <mychael.jared29@gmail.com>
2018-01-25 17:59:20 -0500

    modificando

--- golterra_v1/json/getAll_Users.php
+++ golterra_v1/json/getAll_Users.php
@@ -2,14 +2,27 @@
 /**
  * Obtiene todas los usuarios de la base de datos
  */
-require 'users.php';
+//require 'users.php';
+include('../config.php') ;
+try{
+    $conexion = new PDO($host,$usernameserver,$passwordserver);
+    $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion -> exec("SET CHARACTER SET UTF8");
+}catch(Exception $e){
+    die("Error " . $e->getMessage());
+    echo "Linea del error " . $e->getLine();
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     // Manejar petición GET
-    $usuario = Users::getAll();
-    if ($usuario) {
-        $datos["estado"] = 1;
-        $datos["usuario"] = $usuario;
-        print json_encode($datos);
+    $sql_total="SELECT username, email, photo, nombres FROM usuarios";
+	$resultado=$conexion->prepare($sql_total);
+    $resultado->execute(array());
+
+    if ($resultado->fetch(PDO::FETCH_ASSOC)) {
+        $row = $resultado->fetch(PDO::FETCH_ASSOC);
+        $json['usuario'][]=$row;
+        print json_encode($json);
     } else {
         print json_encode(array(
             "estado" => 2,
