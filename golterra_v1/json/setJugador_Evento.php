25d0046add6d6be156b0e1e10212ac5aad9fefaf
Maycol Meza <mychael.jared29@gmail.com>
2018-02-08 05:52:08 -0500

    JL

--- golterra_v1/json/setJugador_Evento.php
+++ golterra_v1/json/setJugador_Evento.php
@@ -12,41 +12,23 @@ try{
 if(true) {
     //isset($_POST['id_evento'],$_POST['nombres'],$_POST['id_usuario'],$_POST['url_photo'],$_POST['equipo'])
     $id_evento = $_POST['id_evento'];
-    $id_evento = (int) $id_evento;
+    //$id_evento = (int) $id_evento;
     $nombres = $_POST['nombres'];
     $id_usuario = $_POST['id_usuario'];
-    $id_usuario = (int) $id_usuario;
+    //$id_usuario = (int) $id_usuario;
     $rango = $_POST['rango'];
     $experiencia = $_POST['experiencia'];
     $url_photo = $_POST['url_photo'];
     $equipo = $_POST['equipo'];
    
     //FALTA EL TITULO Y DESCRIPCION
-    $query = "Select * from jugadores_evento where id_evento = $id_evento and id_usuario = $id_usuario";
+    $query = "Select * from jugadores_evento where id_evento='$id_evento' and id_usuario='$id_usuario'";
     $result = $conexion->prepare($query);
-    //$result->execute();
+    $result->execute();
 
-    If($result->execute()){
-        $query = "insert into jugadores_evento (id_evento,nombres,id_usuario,rango,experiencia,url_photo,equipo) values (?,?,?,?,?,?.?)";
-        $inserted_coordenadas = $conexion->prepare($query);
-        //$inserted->bindParam('ssss',$email,$password,$url_image,$nombres);//estaba con bind_param
-        $inserted_coordenadas->bindParam(1, $id_evento, PDO::PARAM_STR); 
-        $inserted_coordenadas->bindParam(2, $nombres, PDO::PARAM_STR);
-        $inserted_coordenadas->bindParam(3, $id_usuario, PDO::PARAM_STR);
-        $inserted_coordenadas->bindParam(4, $rango, PDO::PARAM_STR);
-        $inserted_coordenadas->bindParam(5, $experiencia, PDO::PARAM_STR);
-        $inserted_coordenadas->bindParam(6, $url_photo, PDO::PARAM_STR);
-        $inserted_coordenadas->bindParam(7, $equipo, PDO::PARAM_STR);
-        $inserted_coordenadas->execute();
-      
-        $json['success'] = 'Apuntado';
-        echo json_encode($json);
-    }else{
-        
-        $json['success'] = 'Ya estas apuntado al partido';
-        echo json_encode($json);
-    }
-    /*
+    $json['success'] = 'antes del if';
+    echo json_encode($json);
+    
     if($result->rowCount() == 1){
         
           $json['success'] = 'Ya estas apuntado al partido';
@@ -70,7 +52,7 @@ if(true) {
           echo json_encode($json);
           //mysqli_close($this -> conexion); // buscar otra forma de cerrar la conexion, y si es necesario aqui
     }
-*/
+
     
 }
 
