ee5bf609d67e8a999e1eabe8e4f270111f3f6065
Maycol Meza <mychael.jared29@gmail.com>
2018-02-08 04:50:47 -0500

    fd

--- golterra_v1/json/setJugador_Evento.php
+++ golterra_v1/json/setJugador_Evento.php
@@ -11,17 +11,17 @@ try{
 
 if(isset($_POST['id_evento'],$_POST['nombres'],$_POST['id_usuario'],$_POST['url_photo'],$_POST['equipo'])) {
     $id_evento = $_POST['id_evento'];
-    $id_evento = (int) $id_evento;
+    //$id_evento = (int) $id_evento;
     $nombres = $_POST['nombres'];
     $id_usuario = $_POST['id_usuario'];
-    $id_usuario = (int) $id_usuario;
+    //$id_usuario = (int) $id_usuario;
     $rango = $_POST['rango'];
     $experiencia = $_POST['experiencia'];
     $url_photo = $_POST['url_photo'];
     $equipo = $_POST['equipo'];
 
     //FALTA EL TITULO Y DESCRIPCION
-    $query = "Select * from jugadores_evento where id_evento=$id_evento and id_usuario=$id_usuario";
+    $query = "Select * from jugadores_evento where id_evento='$id_evento' and id_usuario='$id_usuario'";
     $result = $conexion->prepare($query);
     $result->execute();
 
@@ -35,9 +35,9 @@ if(isset($_POST['id_evento'],$_POST['nombres'],$_POST['id_usuario'],$_POST['url_
           $query = "insert into jugadores_evento (id_evento,nombres,id_usuario,rango,experiencia,url_photo,equipo) values (?,?,?,?,?,?.?)";
           $inserted_coordenadas = $conexion->prepare($query);
           //$inserted->bindParam('ssss',$email,$password,$url_image,$nombres);//estaba con bind_param
-          $inserted_coordenadas->bindParam(1, $id_evento, PDO::PARAM_INT); 
+          $inserted_coordenadas->bindParam(1, $id_evento, PDO::PARAM_STR); 
           $inserted_coordenadas->bindParam(2, $nombres, PDO::PARAM_STR);
-          $inserted_coordenadas->bindParam(3, $id_usuario, PDO::PARAM_INT);
+          $inserted_coordenadas->bindParam(3, $id_usuario, PDO::PARAM_STR);
           $inserted_coordenadas->bindParam(4, $rango, PDO::PARAM_STR);
           $inserted_coordenadas->bindParam(5, $experiencia, PDO::PARAM_STR);
           $inserted_coordenadas->bindParam(6, $url_photo, PDO::PARAM_STR);
