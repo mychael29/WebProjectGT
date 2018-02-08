84c93b0b724b57f0a5f4b3b03a4ebe8c94c1f58e
Maycol Meza <mychael.jared29@gmail.com>
2018-02-08 06:00:12 -0500

    j

--- golterra_v1/json/setJugador_Evento.php
+++ golterra_v1/json/setJugador_Evento.php
@@ -28,30 +28,47 @@ if(true) {
 
     $json['success'] = 'antes del if';
     echo json_encode($json);
-    
-    if($result->rowCount() == 1||$result->rowCount()!=null){
-        
-          $json['success'] = 'Ya estas apuntado al partido';
-          echo json_encode($json);
-          //mysqli_close($this -> conexion); // buscar otra forma de cerrar la conexion, y si es necesario aqui
+    if($result->rowCount()==null){
+        $query = "insert into jugadores_evento (id_evento,nombres,id_usuario,rango,experiencia,url_photo,equipo) values (?,?,?,?,?,?.?)";
+        $inserted_coordenadas = $conexion->prepare($query);
+        //$inserted->bindParam('ssss',$email,$password,$url_image,$nombres);//estaba con bind_param
+        $inserted_coordenadas->bindParam(1, $id_evento, PDO::PARAM_STR); 
+        $inserted_coordenadas->bindParam(2, $nombres, PDO::PARAM_STR);
+        $inserted_coordenadas->bindParam(3, $id_usuario, PDO::PARAM_STR);
+        $inserted_coordenadas->bindParam(4, $rango, PDO::PARAM_STR);
+        $inserted_coordenadas->bindParam(5, $experiencia, PDO::PARAM_STR);
+        $inserted_coordenadas->bindParam(6, $url_photo, PDO::PARAM_STR);
+        $inserted_coordenadas->bindParam(7, $equipo, PDO::PARAM_STR);
+        $inserted_coordenadas->execute();
+      
+        $json['success'] = 'Apuntado';
+        echo json_encode($json);
     }else{
-          //registro
-          $query = "insert into jugadores_evento (id_evento,nombres,id_usuario,rango,experiencia,url_photo,equipo) values (*,?,*,?,?,?.?)";
-          $inserted_coordenadas = $conexion->prepare($query);
-          //$inserted->bindParam('ssss',$email,$password,$url_image,$nombres);//estaba con bind_param
-          $inserted_coordenadas->bindParam(1, $id_evento, PDO::PARAM_INT); 
-          $inserted_coordenadas->bindParam(2, $nombres, PDO::PARAM_STR);
-          $inserted_coordenadas->bindParam(3, $id_usuario, PDO::PARAM_INT);
-          $inserted_coordenadas->bindParam(4, $rango, PDO::PARAM_STR);
-          $inserted_coordenadas->bindParam(5, $experiencia, PDO::PARAM_STR);
-          $inserted_coordenadas->bindParam(6, $url_photo, PDO::PARAM_STR);
-          $inserted_coordenadas->bindParam(7, $equipo, PDO::PARAM_STR);
-          $inserted_coordenadas->execute();
-        
-          $json['success'] = 'Apuntado';
-          echo json_encode($json);
-          //mysqli_close($this -> conexion); // buscar otra forma de cerrar la conexion, y si es necesario aqui
+        if($result->rowCount() == 1){
+            
+              $json['success'] = 'Ya estas apuntado al partido';
+              echo json_encode($json);
+              //mysqli_close($this -> conexion); // buscar otra forma de cerrar la conexion, y si es necesario aqui
+        }else{
+              //registro
+              $query = "insert into jugadores_evento (id_evento,nombres,id_usuario,rango,experiencia,url_photo,equipo) values (?,?,?,?,?,?.?)";
+              $inserted_coordenadas = $conexion->prepare($query);
+              //$inserted->bindParam('ssss',$email,$password,$url_image,$nombres);//estaba con bind_param
+              $inserted_coordenadas->bindParam(1, $id_evento, PDO::PARAM_STR); 
+              $inserted_coordenadas->bindParam(2, $nombres, PDO::PARAM_STR);
+              $inserted_coordenadas->bindParam(3, $id_usuario, PDO::PARAM_STR);
+              $inserted_coordenadas->bindParam(4, $rango, PDO::PARAM_STR);
+              $inserted_coordenadas->bindParam(5, $experiencia, PDO::PARAM_STR);
+              $inserted_coordenadas->bindParam(6, $url_photo, PDO::PARAM_STR);
+              $inserted_coordenadas->bindParam(7, $equipo, PDO::PARAM_STR);
+              $inserted_coordenadas->execute();
+            
+              $json['success'] = 'Apuntado';
+              echo json_encode($json);
+              //mysqli_close($this -> conexion); // buscar otra forma de cerrar la conexion, y si es necesario aqui
+        }
     }
+    
 
     
 }
