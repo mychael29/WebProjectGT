f3225dfc34fdece2df4e60f26afbb88cbd7b8af8
Maycol Meza <mychael.jared29@gmail.com>
2018-02-12 03:18:14 -0500

    mod

--- golterra_v1/json/AllEvento_Items.php
+++ golterra_v1/json/AllEvento_Items.php
@@ -10,24 +10,16 @@ try{
     die("Error " . $e->getMessage());
     echo "Linea del error " . $e->getLine();
 }
-$query = "SELECT * FROM evento_prueba";
 
-try {
-    $result = $conexion->prepare($query);
-    $result->execute();
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+    $eventos=array();
 
-    while($row = $result->fetch(PDO::FETCH_ASSOC)){
-        $json['items_evento'][] = $row;
+    $consulta=$conexion->query("SELECT * FROM evento_prueba");
+    while($evento=$consulta->fetch(PDO::FETCH_ASSOC)){
+        
+        $eventos[]=$evento;
     }
-
-    echo json_encode($json);
-                    
-} catch (PDOException $e) {
-    $json['error'] = 'exception';
-    echo json_encode($json);
-    // Aquí puedes clasificar el error dependiendo de la excepción
-    // para presentarlo en la respuesta Json
-    return -1;
+    print json_encode($eventos);
 }
 	
 ?>
