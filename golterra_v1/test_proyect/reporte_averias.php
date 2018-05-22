5a122697b3751e936185172aa0198f7d36c8d2ae
Maycol Meza <mychael.jared29@gmail.com>
2018-05-21 22:07:57 -0500

    Oj

--- golterra_v1/test_proyect/reporte_averias.php
+++ golterra_v1/test_proyect/reporte_averias.php
@@ -99,7 +99,7 @@
             
         <?php 
         echo "report error1";
-        include('controllers/averias_controller.php');
+        //include('controllers/averias_controller.php');
         echo "report error2"; ?>
 
         </div>
