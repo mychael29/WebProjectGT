b75939c3c8d88d88d0b474086bc853ef32cf4fe9
Maycol Meza <mychael.jared29@gmail.com>
2018-07-10 02:46:31 -0500

    codes random

---
+++ golterra_v1/lp3_code_random.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Maycol Meza</title>
+</head>
+<body>
+<p>ACCESO VIVIENDA</p>
+<br>
+<table width="100%" border="1" cellpadding="0" cellspacing="0" bordercolor="#000000">
+
+    <tr>
+        <th style="background: cornflowerblue">7.1. Tenencia</th>
+        <th style="background: cornflowerblue" colspan="2" >7.2.Material de Construcción</th>
+        <th style="background: cornflowerblue">7.3. Estado de Conservadón</th>
+        <th style="background: cornflowerblue">7.4.Equipamiento de la Vivienda</th>
+    </tr>
+
+    <tr>
+        <td rowspan="2"><input type="radio" name="optradioTenencia">Dueño /Propietario</label></td>
+        <td style="background: cornflowerblue">Pared:</td>
+        <td style="background: cornflowerblue">Piso:</td>
+        <td rowspan="2"><input type="radio" name="optradioEstado">Nueva</label></td>
+        <td rowspan="2"><input type="radio" name="optradioEquipamiento">Automovil de uso personal</label></td>
+    </tr>
+
+    <tr>
+        <td><input type="radio" name="optradioPared">Noble</label></td>
+        <td><input type="radio" name="optradioPiso">Parquet</label></td>
+
+    </tr>
+    <tr>
+        <td><input type="radio" name="optradioTenencia">Propia con Crédito Hipotecario</label></td>
+        <td><input type="radio" name="optradioPared">Adobe</label></td>
+        <td><input type="radio" name="optradioPiso">Cerámica</label></td>
+        <td><input type="radio" name="optradioEstado">Buena</label></td>
+        <td><input type="radio" name="optradioEquipamiento">Electrodomésticos</label></td>
+    </tr>
+    <tr>
+        <td><input type="radio" name="optradioTenencia">Alquilada</label></td>
+        <td><input type="radio" name="optradioPared">Madera</label></td>
+        <td><input type="radio" name="optradioPiso">Loseta</label></td>
+        <td><input type="radio" name="optradioEstado">Conservada</label></td>
+        <td><input type="radio" name="optradioEquipamiento">Cable</label></td>
+    </tr>
+    <tr>
+        <td><input type="radio" name="optradioTenencia">Alojado con Familiar</label></td>
+        <td><input type="radio" name="optradioPared">Quincha</label></td>
+        <td><input type="radio" name="optradioPiso">Vinílico</label></td>
+        <td><input type="radio" name="optradioEstado">Remodelada</label></td>
+        <td><input type="radio" name="optradioEquipamiento">Internet</label></td>
+    </tr>
+    <tr>
+        <td><input type="radio" name="optradioTenencia">Cedida por persona, institución</label></td>
+        <td><input type="radio" name="optradioPared">Esteras</label></td>
+        <td><input type="radio" name="optradioPiso">Cemento</label></td>
+        <td><input type="radio" name="optradioEstado">Deteriorada</label></td>
+        <td><input type="radio" name="optradioEquipamiento">Celular</label></td>
+    </tr>
+    <tr>
+        <td rowspan="2"><input type="radio" name="optradioTenencia">Alojado con personas no familiares</label></td>
+        <td style="background: cornflowerblue">Techo:</td>
+        <td rowspan="2"><input type="radio" name="optradioPiso">Madera</label></td>
+        <td rowspan="7">&nbsp;</td>
+        <td rowspan="2"><input type="radio" name="optradioEquipamiento">Equipo de Cómputo-Laptop</label></td>
+    </tr>
+    <tr>
+        <td><input type="radio" name="optradioTecho">Concreto</label></td>
+    </tr>
+    <tr>
+        <td><input type="radio" name="optradioTenencia">Guardianía</label></td>
+        <td><input type="radio" name="optradioTecho">Calamina</label></td>
+        <td><input type="radio" name="optradioPiso">Falso Piso</label></td>
+        <td rowspan="5"><input type="radio" name="optradioEquipamiento">Otros</label></td>    </tr>
+    <tr>
+        <td><input type="radio" name="optradioTenencia">Hipotecada por deudas y/o judicial</label></td>
+        <td><input type="radio" name="optradioTecho">Eternit Madera</label></td>
+        <td><input type="radio" name="optradioPiso">Tierra</label></td>
+    </tr>
+    <tr>
+        <td><input type="radio" name="optradioTenencia">Comparte con amigos</label></td>
+        <td><input type="radio" name="optradioTecho">Esteras</label></td>
+        <td rowspan="3"><input type="radio" name="optradioPiso">Otros</label></td>
+    </tr>
+    <tr>
+        <td rowspan="2"><input type="radio" name="optradioTenencia">Otros</label></td>
+        <td><input type="radio" name="optradioTecho">Plástico</label></td>
+    </tr>
+    <tr>
+        <td><input type="radio" name="optradioTecho">Cartones</label></td>
+    </tr>
+
+</table>
+</body>
+</html>
