62ccf20661461cdd544f189b5da93167dd5be8ac
Maycol Meza <mychael.jared29@gmail.com>
2019-02-01 03:27:59 -0500

    hio

--- golterra_v1/json/get_events.php
+++ golterra_v1/json/get_events.php
@@ -17,11 +17,11 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     while($evento=$consultaEvent->fetch(PDO::FETCH_ASSOC)){
         //echo "string : ".$evento[]['id_event'];
 
-        var_dump[$evento];
+        //var_dump[$evento];
 
         $events['info']=$evento;
         $consultaJugador=$conexion->query("SELECT * FROM jugadores_evento WHERE id_evento=".$evento['id_event']);
-        while($jugador=$consultaJugador>fetch(PDO::FETCH_ASSOC)){
+        while($jugador=$consultaJugador->fetch(PDO::FETCH_ASSOC)){
             $events['players'][]=$jugador;
         }
     }
